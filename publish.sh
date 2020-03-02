2634970691e0d50500807cdf6111a4b37152c9b7
Ginkgoch <ginkgoch@outlook.com>
2020-03-02 11:30:25 +0800

    Update website.

--- publish.sh
+++ publish.sh
@@ -1,4 +1,7 @@
 cp -r ../../gitee.com/ginkgoch-website/dist/* ./
+echo "sync website source code complete, ready to push..."
+
 git add .
 git commit -m "Update website."
 git push origin master
+echo "repo pushed, check the website in few minutes"
